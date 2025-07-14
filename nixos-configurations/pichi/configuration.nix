534b5d713c6a6d1f9fe56fe11c61f9811f91f601
muere4 <muere4@gmail.com>
2025-07-14 00:23:53 -0300

    formateo configuration.nix de pichi

--- nixos-configurations/pichi/configuration.nix
+++ nixos-configurations/pichi/configuration.nix
@@ -5,160 +5,154 @@
 { config, pkgs, inputs, unstable, ... }:
 
 {
-
+  # Habilitar funciones experimentales de Nix
   nix.settings.experimental-features = ["nix-command" "flakes"];
 
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+  imports = [
+    # Incluir los resultados del escaneo de hardware
+    ./hardware-configuration.nix
+  ];
 
-  # Bootloader.
+  # Configuración del bootloader
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
-  #networking.nameservers = [ "1.1.1.1" "1.0.0.1" ];
-
-  # Set your time zone.
-  time.timeZone = "America/Argentina/Buenos_Aires";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "es_AR.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "es_AR.UTF-8";
-    LC_IDENTIFICATION = "es_AR.UTF-8";
-    LC_MEASUREMENT = "es_AR.UTF-8";
-    LC_MONETARY = "es_AR.UTF-8";
-    LC_NAME = "es_AR.UTF-8";
-    LC_NUMERIC = "es_AR.UTF-8";
-    LC_PAPER = "es_AR.UTF-8";
-    LC_TELEPHONE = "es_AR.UTF-8";
-    LC_TIME = "es_AR.UTF-8";
+  # Configuración de red
+  networking = {
+    hostName = "nixos";
+    networkmanager.enable = true;
+    # networking.wireless.enable = true;  # Habilita soporte wireless via wpa_supplicant
+    # networking.nameservers = [ "1.1.1.1" "1.0.0.1" ];
   };
 
-  # Enable the X11 windowing system.
-  # You can disable this if you're only using the Wayland session.
-  services.xserver.enable = true;
+  # Configuración de zona horaria
+  time.timeZone = "America/Argentina/Buenos_Aires";
 
-  # Enable the KDE Plasma Desktop Environment.
+  # Configuración de idioma e internacionalización
+  i18n = {
+    defaultLocale = "es_AR.UTF-8";
+    extraLocaleSettings = {
+      LC_ADDRESS = "es_AR.UTF-8";
+      LC_IDENTIFICATION = "es_AR.UTF-8";
+      LC_MEASUREMENT = "es_AR.UTF-8";
+      LC_MONETARY = "es_AR.UTF-8";
+      LC_NAME = "es_AR.UTF-8";
+      LC_NUMERIC = "es_AR.UTF-8";
+      LC_PAPER = "es_AR.UTF-8";
+      LC_TELEPHONE = "es_AR.UTF-8";
+      LC_TIME = "es_AR.UTF-8";
+    };
+  };
+
+  # Configuración del servidor X11
+  services.xserver = {
+    enable = true;
+    xkb = {
+      layout = "es";
+      variant = "";
+    };
+  };
+
+  # Configuración del display manager y entorno de escritorio
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
 
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "es";
-    variant = "";
-  };
-
-  # Configure console keymap
+  # Configuración de keymap de consola
   console.keyMap = "es";
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
+  # Servicios habilitados
+  services = {
+    printing.enable = true;
+    pulseaudio.enable = false;
+    # Habilitar touchpad support (enabled default in most desktopManager)
+    # xserver.libinput.enable = true;
+  };
 
-  # Enable sound with pipewire.
-  services.pulseaudio.enable = false;
+  # Configuración de audio con PipeWire
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
+    # Si quieres usar aplicaciones JACK, descomenta esto
+    # jack.enable = true;
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with 'passwd'.
+  # Configuración de usuarios
   users.users.muere = {
     isNormalUser = true;
     description = "muere";
-    extraGroups = [ "networkmanager" "wheel" ];
+    extraGroups = ["networkmanager" "wheel"];
     packages = with pkgs; [
       kdePackages.kate
       # Ejemplo de paquete del canal unstable:
       # unstable.discord
-    #  thunderbird
+      # thunderbird
     ];
   };
 
-  # Install firefox.
+  # Configuración de programas
   programs.firefox.enable = true;
-  #default browser ms-edge
+
+  # Variables de entorno
   environment.sessionVariables = {
     DEFAULT_BROWSER = "${pkgs.microsoft-edge}/bin/microsoft-edge";
   };
 
-
-  # Allow unfree packages
+  # Configuración de paquetes
   nixpkgs.config.allowUnfree = true;
 
-  # List packages installed in system profile. To search, run:
-
-
-
-  # $ nix search wget
+  # Paquetes del sistema
   environment.systemPackages = with pkgs; [
-    vim # Editor básico
+    # Editores y herramientas básicas
+    vim
     wget
     git
+
+    # Navegadores
+    microsoft-edge
+
     # Ejemplo de cómo usar paquetes del canal unstable:
     # unstable.neovim
-
-    microsoft-edge
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
+  # Configuración XDG para aplicaciones por defecto
+  xdg.mime = {
+    enable = true;
+    defaultApplications = {
+      "application/pdf" = "microsoft-edge.desktop";
+      "text/html" = "microsoft-edge.desktop";
+      "x-scheme-handler/http" = "microsoft-edge.desktop";
+      "x-scheme-handler/https" = "microsoft-edge.desktop";
+      "x-scheme-handler/about" = "microsoft-edge.desktop";
+      "x-scheme-handler/unknown" = "microsoft-edge.desktop";
+    };
+  };
+
+  # Configuración de programas adicionales
   # programs.mtr.enable = true;
   # programs.gnupg.agent = {
   #   enable = true;
   #   enableSSHSupport = true;
   # };
 
-  # List services that you want to enable:
-  # XDG default applications
-  xdg.mime.enable = true;
-  xdg.mime.defaultApplications = {
-    "application/pdf" = "microsoft-edge.desktop";
-    "text/html" = "microsoft-edge.desktop";
-    "x-scheme-handler/http" = "microsoft-edge.desktop";
-    "x-scheme-handler/https" = "microsoft-edge.desktop";
-    "x-scheme-handler/about" = "microsoft-edge.desktop";
-    "x-scheme-handler/unknown" = "microsoft-edge.desktop";
-  };
-
-  # Enable the OpenSSH daemon.
+  # Configuración de servicios de red
   # services.openssh.enable = true;
 
-  # Open ports in the firewall.
+  # Configuración de firewall
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
+  # O deshabilitar el firewall completamente:
   # networking.firewall.enable = false;
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It's perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.05"; # Did you read the comment?
+  # Este valor determina la versión de NixOS desde la cual se tomaron
+  # las configuraciones por defecto para datos con estado, como ubicaciones
+  # de archivos y versiones de base de datos en tu sistema. Es perfectamente
+  # correcto y recomendado dejar este valor en la versión de lanzamiento
+  # de la primera instalación de este sistema.
+  # Antes de cambiar este valor lee la documentación para esta opción
+  # (ej. man configuration.nix o en https://nixos.org/nixos/options.html).
+  system.stateVersion = "25.05"; # ¿Leíste el comentario?
 
 }
