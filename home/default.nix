b4c1e427a8553d11b05ac8a1f13e61e03d065882
muere4 <muere4@gmail.com>
2025-12-22 19:21:38 -0300

    Se agrego un hardare-cinfighuration.nix nuevo, se volvio a edge

--- home/default.nix
+++ home/default.nix
@@ -14,16 +14,16 @@
     kdePackages.arianna
   ];
 
-   xdg.mimeApps = {
-        enable = true;
-        defaultApplications = {
-          "text/html" = [ "firefox.desktop" ];
-          "x-scheme-handler/http" = [ "firefox.desktop" ];
-          "x-scheme-handler/https" = [ "firefox.desktop" ];
-          "x-scheme-handler/about" = [ "firefox.desktop" ];
-          "x-scheme-handler/unknown" = [ "firefox.desktop" ];
-        };
-      };
+#    xdg.mimeApps = {
+#         enable = true;
+#         defaultApplications = {
+#           "text/html" = [ "firefox.desktop" ];
+#           "x-scheme-handler/http" = [ "firefox.desktop" ];
+#           "x-scheme-handler/https" = [ "firefox.desktop" ];
+#           "x-scheme-handler/about" = [ "firefox.desktop" ];
+#           "x-scheme-handler/unknown" = [ "firefox.desktop" ];
+#         };
+#       };
 
 
   programs.direnv = {
--- modules/desktop/browsers/microsoft-edge.nix
+++ modules/desktop/browsers/microsoft-edge.nix
@@ -26,16 +26,16 @@ in
       };
 
       # Archivos XDG
-#       xdg.mimeApps = {
-#         enable = true;
-#         defaultApplications = {
-#           "text/html" = [ "microsoft-edge.desktop" ];
-#           "x-scheme-handler/http" = [ "microsoft-edge.desktop" ];
-#           "x-scheme-handler/https" = [ "microsoft-edge.desktop" ];
-#           "x-scheme-handler/about" = [ "microsoft-edge.desktop" ];
-#           "x-scheme-handler/unknown" = [ "microsoft-edge.desktop" ];
-#         };
-#       };
+      xdg.mimeApps = {
+        enable = true;
+        defaultApplications = {
+          "text/html" = [ "microsoft-edge.desktop" ];
+          "x-scheme-handler/http" = [ "microsoft-edge.desktop" ];
+          "x-scheme-handler/https" = [ "microsoft-edge.desktop" ];
+          "x-scheme-handler/about" = [ "microsoft-edge.desktop" ];
+          "x-scheme-handler/unknown" = [ "microsoft-edge.desktop" ];
+        };
+      };
     };
   };
 }
